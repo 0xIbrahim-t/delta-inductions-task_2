9b61af5f98cde0706df368773f6b67597e528aba
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-14 21:36:14 +0530

    Update submitTask.sh

--- app/submitTask.sh
+++ app/submitTask.sh
@@ -4,7 +4,6 @@
 
 
 if [ -f ~/domain_pref.txt ]; then
-	rollnumber=$(awk 'NR==1 {print; exit}' "$HOME/rollnumber.txt")
 	tasks_completed=(n n n n n n n n n)
 	mentee_dom=()
 	for d in sysad web app; do
@@ -64,8 +63,6 @@ elif [ -f ~/allocatedMentees.txt ]; then
 	domain=$(awk "NR==$line {print \$3}" ~/allocatedMentees.txt)
 	echo $domain
 	for allocated_mentee in ${allocated_mentees[@]}; do
-		echo $allocated_mentee
-		echo $domain
 		task_completed=(n n n)
 		for h in 1 2 3; do
 
@@ -84,46 +81,19 @@ elif [ -f ~/allocatedMentees.txt ]; then
 			fi
 		done
 
-		if [ -s $(eval echo ~$allocated_mentee)/task_completed.txt ]; then
-			readarray -t contents_task_completed < $(eval echo ~$allocated_mentee)/task_completed.txt
-			x=0
-			case $domain in
-				"sysad") x=1;;
-				"web") x=5;;
-				"app") x=9;;
-			esac
-			rm -R $(eval echo ~$allocated_mentee)/task_completed.txt
-
-			for line_task_completed in ${contents_task_completed[@]};do
-				if [[ $line_task_completed==${content_task_completed[$x]} ]]; then
-					echo "${line_task_completed:0:$((${#line_task_completed}-1))}${task_completed[0]}" >> $(eval echo ~$allocated_mentee)/task_completed.txt
-
-				elif [[ $line_task_completed==${content_task_completed[$(($x+1))]} ]]; then
-					echo "${line_task_completed:0:$((${#line_task_completed}-1))}${task_completed[1]}" >> $(eval echo ~$allocated_mentee)/task_completed.txt
-
-				elif [[ $line_task_completed==${content_task_completed[$(($x+2))]} ]]; then
-					echo "${line_task_completed:0:$((${#line_task_completed}-1))}${task_completed[2]}" >> $(eval echo ~$allocated_mentee)/task_completed.txt
-
-				else
-					echo $line_task_completed >> $(eval echo ~$allocated_mentee)/task_completed.txt
-
-				fi
-			done
-		else
-			v=0
-			for g in SysAd Web App; do
-				echo "$g:" >> $(eval echo ~$allocated_mentee)/task_completed.txt
-				for k in Task1: Task2: Task3:; do
-					v=$(($v+1))
-					if [ ${g,,}==$domain ]; then
-						r=${task_completed[v]}
-					else
-						r=n
-					fi
-					echo "    $k: $r" >> $(eval echo ~$allocated_mentee)/task_completed.txt
-				done
-			done
-		fi
+		case $domain in
+			"sysad") allocatedDomain=Sysad;;
+			"web") allocatedDomain=Web;;
+			"app") allocatedDomain=App;;
+		esac
+  		export allocatedDomain
+  		completedTask=""
+  		for i in ${task_completed[@]}; do
+    			completedTask="${completedTask}${i}"
+    		done
+      		export completedTask
+		export allocated_mentee
+  		python3 /scripts/completed_task.py
 	done
 else
 	echo "you are neither a mentee nor a mentor, and hence cannot run this command"
