08f70637352185d9c9032bc3f9f5862fbc76e422
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-14 20:43:35 +0530

    Create submitted_task.py

---
+++ app/submitted_task.py
@@ -0,0 +1,17 @@
+import mysql.connector
+import os
+
+domain = os.getenv('doma')
+name = os.getenv('USER')
+rollnum = os.getenv('rollnumber')
+submitted_Task = os.getenv('submittedTask')
+db_host = os.getenv('dbHost')
+
+mydb = mysql.connector.connect(host=db_host,user="username",password="password",database="db_name")
+mycursor = mydb.cursor()
+
+mycursor.execute(f"DROP TABLE IF EXISTS {name}_task_submitted")
+mycursor.execute(f"create table {name}_task_submitted (Task_number INT, Sysad VARCHAR(1), Web VARCHAR(1), App VARCHAR(1))")
+
+for num in [0, 1, 2]:
+    mycursor.execute(f"INSERT INTO {name}_task_submitted (Task_number, Sysad, Web, App) Values ({num}, {submitted_Task[num]}, {submitted_Task[num+3]}, {submitted_Task[num+6]})")
