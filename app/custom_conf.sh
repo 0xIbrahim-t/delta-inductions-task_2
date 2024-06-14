c0bba36fbc73d56d66b7fd252f4ae7ba229d4978
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-15 03:21:09 +0530

    Update custom_conf.sh

--- app/custom_conf.sh
+++ app/custom_conf.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-echo "<VirtualHost *:80>" > /etc/apache2/sites-available/000-default.conf
-echo "    DocumentRoot ~Core/mentees_domain.txt" >> /etc/apache2/sites-available/000-default.conf
-echo "" >> /etc/apache2/sites-available/000-default.conf
-echo "    <Directory ~Core/mentees_domain.txt>" >> /etc/apache2/sites-available/000-default.conf
-echo "        Options Indexes FollowSymLinks" >> /etc/apache2/sites-available/000-default.conf
-echo "        AllowOverride None" >> /etc/apache2/sites-available/000-default.conf
-echo "        Require all granted" >> /etc/apache2/sites-available/000-default.conf
-echo "    </Directory>" >> /etc/apache2/sites-available/000-default.conf
-echo "" >> /etc/apache2/sites-available/000-default.conf
-echo "    ErrorLog ${APACHE_LOG_DIR}/error.log" >> /etc/apache2/sites-available/000-default.conf
-echo "    CustomLog ${APACHE_LOG_DIR}/access.log combined" >> /etc/apache2/sites-available/000-default.conf
-echo "</VirtualHost>" >> /etc/apache2/sites-available/000-default.conf
+touch /etc/apache2/sites-available/app.conf
+echo "<VirtualHost *:80>
+     ServerAdmin webmaster@app
+     ServerName app
+     ServerAlias www.app
+     DocumentRoot /core
+     DirectoryIndex mentees_domain.txt
+</VirtualHost>" >> /etc/apache2/sites-available/app.conf
+echo "ServerName app" >> /etc/apache2/apache2.conf
+service apache2 start
+a2ensite app.conf
+a2dissite 000-default.conf
+service apache2 reload
