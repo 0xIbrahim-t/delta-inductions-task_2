7d13f5e021a7630cce2055d1e03173d3d03563e5
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-15 16:49:59 +0530

    Update submitted_task.py

--- app/submitted_task.py
+++ app/submitted_task.py
@@ -7,11 +7,10 @@ submitted_Task = os.getenv('submittedTask')
 mydb = mysql.connector.connect(host="db",user="username",password="password",database="db_name")
 mycursor = mydb.cursor()
 
-mycursor.execute(f"DROP TABLE IF EXISTS {name}_task_submitted")
-mycursor.execute(f"create table {name}_task_submitted (Task_number INT, Sysad VARCHAR(1), Web VARCHAR(1), App VARCHAR(1))")
-
 for num in [0, 1, 2]:
-    mycursor.execute(f"INSERT INTO {name}_task_submitted (Task_number, Sysad, Web, App) Values ({num + 1}, {submitted_Task[num]}, {submitted_Task[num+3]}, {submitted_Task[num+6]})")
+    mycursor.execute(f"UPDATE {name}_task_submitted SET Sysad = '{submitted_Task[num]}' WHERE Task_number = {num + 1};")
+    mycursor.execute(f"UPDATE {name}_task_submitted SET Web = '{submitted_Task[num + 3]}' WHERE Task_number = {num + 1};")
+    mycursor.execute(f"UPDATE {name}_task_submitted SET App = '{submitted_Task[num + 6]}' WHERE Task_number = {num + 1};")
 
 mycursor.close()
 mydb.close()
