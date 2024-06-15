9f2e0f1d598e80a0130980a728c7d2a567dc8bc2
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-15 12:12:13 +0530

    Update starting.sh

--- app/starting.sh
+++ app/starting.sh
@@ -3,10 +3,10 @@
 
 while read line; do
     mentee=$(echo "$line" | awk '{print $1}')
- 	  mentee_db=$mentee
-  	export mentee_db
- 	  python3 /scripts/start_db_1.py
-  	python3 /scripts/start_db_2.py
+    mentee_db=$mentee
+    export mentee_db
+    python3 /scripts/start_db_1.py
+    python3 /scripts/start_db_2.py
 done < ~Core/menteeDetails.txt
 
 tail -f /dev/null
