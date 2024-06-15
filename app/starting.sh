73cd2e91ebab2d9c95fcb6b1510bb099d1ca8741
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-15 12:11:31 +0530

    Update starting.sh

--- app/starting.sh
+++ app/starting.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 
-
+while read line; do
+    mentee=$(echo "$line" | awk '{print $1}')
+ 	  mentee_db=$mentee
+  	export mentee_db
+ 	  python3 /scripts/start_db_1.py
+  	python3 /scripts/start_db_2.py
+done < ~Core/menteeDetails.txt
 
 tail -f /dev/null
