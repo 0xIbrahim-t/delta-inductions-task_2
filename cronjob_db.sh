5170f9a8d3e4212e4b0f761bf27d1d0c961d0a90
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-15 23:44:10 +0530

    Create cronjob_db.sh

---
+++ cronjob_db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+minute=$(date +%M)
+hour=$(date +%H)
+day=$(date +%d)
+weekday=$(date +%u)
+month=$(date +%m)
+
+if { [ "$minute" -eq 3 ] && [ "$hour" -ge 5 ] && [ "$hour" -le 8 ]; } || { [ "$minute" -eq 3 ] && [ "$hour" -eq 15 ] && [ "$day" -eq 1 ]; } || { [ "$minute" -eq 3 ] && [ "$hour" -eq 15 ] && [ "$weekday" -eq 2 ] && [ "$month" -ge 1 ] && [ "$month" -le 7 ]; }; then
+    docker exec db_container /usr/bin/mysqldump -u username --no-tablespaces --password=password Task_db > db/init.sql
+fi
