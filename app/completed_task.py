8987e7fe7a2b9fa8eb53a6f4b46d474cec051061
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-15 16:45:00 +0530

    Update completed_task.py

--- app/completed_task.py
+++ app/completed_task.py
@@ -9,9 +9,6 @@ name = os.getenv('allocated_mentee')
 mydb = mysql.connector.connect(host="db",user="username",password="password",database="Task_db")
 mycursor = mydb.cursor()
 
-mycursor.execute(f"DROP TABLE IF EXISTS {name}_task_completed")
-mycursor.execute(f"create table {name}_task_completed (Task_number INT, Sysad VARCHAR(1), Web VARCHAR(1), App VARCHAR(1))")
-
 for num in [0, 1, 2]:
     mycursor.execute(f"UPDATE {name}_task_completed SET {allocated_domain} = '{completed_Task[num]}' WHERE Task_number = {num + 1};")
 
