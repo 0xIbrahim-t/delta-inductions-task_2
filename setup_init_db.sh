5c4cca81eb15ff040b935c7eb45e239f24859af5
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-21 22:50:40 +0530

    Update setup_init_db.sh

--- setup_init_db.sh
+++ setup_init_db.sh
@@ -2,10 +2,10 @@
 
 while read line; do
 	mentee=$(echo "$line" | awk '{print $1}')
-  echo "create table ${mentee}_task_submitted (Task_number INT, Sysad VARCHAR(3), Web VARCHAR(3), App VARCHAR(3));" >> db/init.sql
-  echo "INSERT INTO ${mentee}_task_submitted (Task_number, Sysad, Web, App) Values (1, "n", "n", "n");" >> db/init.sql
-  echo "INSERT INTO ${mentee}_task_submitted (Task_number, Sysad, Web, App) Values (2, "n", "n", "n");" >> db/init.sql
-  echo "INSERT INTO ${mentee}_task_submitted (Task_number, Sysad, Web, App) Values (3, "n", "n", "n");" >> db/init.sql
+	echo "create table ${mentee}_task_submitted (Task_number INT, Sysad VARCHAR(3), Web VARCHAR(3), App VARCHAR(3));" >> db/init.sql
+	echo "INSERT INTO ${mentee}_task_submitted (Task_number, Sysad, Web, App) Values (1, "n", "n", "n");" >> db/init.sql
+	echo "INSERT INTO ${mentee}_task_submitted (Task_number, Sysad, Web, App) Values (2, "n", "n", "n");" >> db/init.sql
+	echo "INSERT INTO ${mentee}_task_submitted (Task_number, Sysad, Web, App) Values (3, "n", "n", "n");" >> db/init.sql
 done < app/menteeDetails.txt
 
 echo "Database successfully initialized for the mysql container."
