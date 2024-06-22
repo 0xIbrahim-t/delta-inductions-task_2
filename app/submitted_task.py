93f916c242568c11d70bc61d462ed9b2b6a0c136
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-22 15:26:01 +0530

    Update submitted_task.py

--- app/submitted_task.py
+++ app/submitted_task.py
@@ -8,9 +8,10 @@ mydb = mysql.connector.connect(host="db",user="username",password="password",dat
 mycursor = mydb.cursor()
 
 for num in [0, 1, 2]:
-    mycursor.execute(f"UPDATE {name}_task_submitted SET Sysad = '{submitted_Task[num]}' WHERE Task_number = {num + 1};")
-    mycursor.execute(f"UPDATE {name}_task_submitted SET Web = '{submitted_Task[num + 3]}' WHERE Task_number = {num + 1};")
-    mycursor.execute(f"UPDATE {name}_task_submitted SET App = '{submitted_Task[num + 6]}' WHERE Task_number = {num + 1};")
+    mycursor.execute(f"UPDATE {name}_task_submitted SET Sysad = '{submitted_Task[num]}' WHERE Task_number = {num + 1}")
+    mycursor.execute(f"UPDATE {name}_task_submitted SET Web = '{submitted_Task[num + 3]}' WHERE Task_number = {num + 1}")
+    mycursor.execute(f"UPDATE {name}_task_submitted SET App = '{submitted_Task[num + 6]}' WHERE Task_number = {num + 1}")
 
+mydb.commit()
 mycursor.close()
 mydb.close()
