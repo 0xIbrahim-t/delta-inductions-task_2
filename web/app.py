bdec99d606d9c56ff25bc26d0efff4d9d5460b86
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-23 01:37:11 +0530

    Update app.py

--- web/app.py
+++ web/app.py
@@ -33,7 +33,7 @@ def login():
             elif user_type == 'mentee':
                 return redirect(url_for('mentee_dashboard'))
         else:
-            flash('Invalid username or password. Please try again.', 'danger')
+            flash('Invalid username or password. Please try again.')
     return redirect(url_for('index'))
 
 @app.route('/logout')
