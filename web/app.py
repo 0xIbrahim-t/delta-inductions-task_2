f0f8dcfa4a75c61a7d5010c1fef2d06aa808ae83
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-24 00:35:23 +0530

    Update app.py

--- web/app.py
+++ web/app.py
@@ -8,7 +8,7 @@ def connect_db():
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
