df43fe38bec49a23dfe6d93dcbf264725f695f50
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-23 18:43:21 +0530

    Update setup_init_db.sh

--- setup_init_db.sh
+++ setup_init_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "INSERT INTO web_users (rollnumber, username, password, usertype, doamin_1, domain_2, domain_3) Values ("none", "core", "1234567890", "core", "none", "none", "none");" >> db/init.sql
+echo "INSERT INTO web_users (rollnumber, username, password, usertype, doamin_1, domain_2, domain_3, allocated_mentor) Values ("none", "core", "1234567890", "core", "none", "none", "none", "none");" >> db/init.sql
 
 while read line; do
 	mentee=$(echo "$line" | awk '{print $1}')
@@ -10,7 +10,7 @@ while read line; do
 	echo "INSERT INTO ${mentee}_task_submitted (Task_number, Sysad, Web, App) Values (1, "n", "n", "n");" >> db/init.sql
 	echo "INSERT INTO ${mentee}_task_submitted (Task_number, Sysad, Web, App) Values (2, "n", "n", "n");" >> db/init.sql
 	echo "INSERT INTO ${mentee}_task_submitted (Task_number, Sysad, Web, App) Values (3, "n", "n", "n");" >> db/init.sql
- 	echo "INSERT INTO web_users (rollnumber, username, password, usertype, doamin_1, domain_2, domain_3) Values ($rollnumber, $mentee, "1234567890", "mentee", "none", "none", "none");" >> db/init.sql
+ 	echo "INSERT INTO web_users (rollnumber, username, password, usertype, doamin_1, domain_2, domain_3, allocated_mentor) Values ($rollnumber, $mentee, "1234567890", "mentee", "none", "none", "none", "none");" >> db/init.sql
 done < app/menteeDetails.txt
 
 while read line; do
@@ -18,7 +18,7 @@ while read line; do
 	dom=$(echo "$line" | awk '{print $2}')
  	cap=$(echo "$line" | awk '{print $3}')
   	cap="${cap//[$'\n\r\t ']/}"
-	echo "INSERT INTO web_users (rollnumber, username, password, usertype, doamin_1, domain_2, domain_3) Values ($cap, $mentor, "1234567890", "mentor", $dom, "none", "none");" >> db/init.sql
+	echo "INSERT INTO web_users (rollnumber, username, password, usertype, doamin_1, domain_2, domain_3, allocated_mentor) Values ($cap, $mentor, "1234567890", "mentor", $dom, "none", "none", "none");" >> db/init.sql
 done < app/mentorDetails.txt
 
 echo "Database successfully initialized for the mysql container."
