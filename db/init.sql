bd65a5314ec7fbc25e4d448c66aff4cb1ca91a57
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-23 18:42:07 +0530

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -4,7 +4,7 @@ FLUSH PRIVILEGES;
 
 USE Task_db;
 
-create table web_users (rollnumber VARCHAR(20), username VARCHAR(20), password VARCHAR(20), usertype VARCHAR(20), doamin_1 VARCHAR(20), domain_2 VARCHAR(20), domain_3 VARCHAR(20));
+create table web_users (rollnumber VARCHAR(20), username VARCHAR(20), password VARCHAR(20), usertype VARCHAR(20), doamin_1 VARCHAR(20), domain_2 VARCHAR(20), domain_3 VARCHAR(20), allocated_mentor VARCHAR(20));
 
 
 
